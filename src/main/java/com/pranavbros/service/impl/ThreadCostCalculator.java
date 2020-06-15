fce2e59ab6dbd6c85062e701625f0929d21ce273
Rajkumar <monishrajkumar@gmail.com>
2020-06-15 14:48:26 +0530

    Included  loggers

--- src/main/java/com/pranavbros/service/impl/ThreadCostCalculator.java
+++ src/main/java/com/pranavbros/service/impl/ThreadCostCalculator.java
@@ -1,16 +1,31 @@
 package com.pranavbros.service.impl;
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.pranavbros.service.ThreadCostCalculatorService;
 import com.pranavbros.util.Quicksort;
 
 public class ThreadCostCalculator implements ThreadCostCalculatorService{
+	
+	private static final Logger logger = LoggerFactory.getLogger(ThreadCostCalculatorService.class);
 	
 	public int getThreadCost(int[] inputArray) {
 		int arrayLength = inputArray.length;
 		if(arrayLength<2) {
 			return inputArray[0];
 		}else { //(inputArray.length>1) 
+			
+			long startTime = System.currentTimeMillis();
+			logger.info("InputArray -->{}", Arrays.toString(inputArray));
 			Quicksort qs = new Quicksort();
 			qs.sort(inputArray,0, arrayLength-1);
+			long stopTime = System.currentTimeMillis();
+			long elapsedTime = stopTime - startTime;
+			logger.info("Quicksort Time Taken -->{}", elapsedTime);
+			logger.info("SortedArray -->{}" , Arrays.toString(inputArray));
+			
 			int sum = inputArray[0] + inputArray[1];
 			int stepsSum = 0;
 			for(int i=2; i<inputArray.length; i++) {
--- src/test/java/com/pranavbros/ThreadCostCalculatorTest.java
+++ src/test/java/com/pranavbros/ThreadCostCalculatorTest.java
@@ -11,24 +11,35 @@ public class ThreadCostCalculatorTest {
 	@Test
 	public void singleElementArrayCost() {
 		ThreadCostCalculator threadCostCalculator = new ThreadCostCalculator();
-		assertEquals(20, threadCostCalculator.getThreadCost(new int[] {20}));
+		assertEquals(20, threadCostCalculator.getThreadCost(new int[] { 20 }));
 	}
-	
+
 	@Test
 	public void twoElementArrayCost() {
 		ThreadCostCalculator threadCostCalculator = new ThreadCostCalculator();
-		assertEquals(24, threadCostCalculator.getThreadCost(new int[] {20,4}));
+		assertEquals(24, threadCostCalculator.getThreadCost(new int[] { 20, 4 }));
 	}
-	
+
 	@Test
 	public void multipleElementArrayCost() {
 		ThreadCostCalculator threadCostCalculator = new ThreadCostCalculator();
-		assertEquals(54, threadCostCalculator.getThreadCost(new int[] {20,4,8,2}));
+		assertEquals(54, threadCostCalculator.getThreadCost(new int[] { 20, 4, 8, 2 }));
 	}
-	
+
 	@Test
 	public void multipleElementBigArrayCost() {
 		ThreadCostCalculator threadCostCalculator = new ThreadCostCalculator();
-		assertEquals(321, threadCostCalculator.getThreadCost(new int[] {10, 16, 8, 12, 15, 6, 3, 9, 5}));
+		assertEquals(321, threadCostCalculator.getThreadCost(new int[] { 10, 16, 8, 12, 15, 6, 3, 9, 5 }));
+	}
+
+	@Test
+	public void multipleElemenHhugeArrayCost() {
+		ThreadCostCalculator threadCostCalculator = new ThreadCostCalculator();
+		assertEquals(1148179404,
+				threadCostCalculator.getThreadCost(new int[] { 10, 16, 8, 12, 15, 6, 3, 9, 5, 78, 55, 68, 44, 4454, 178,
+						445, 1577, 6698, 1, 111, 157, 178, 487, 877, 1545, 1548, 154545, 155, 57, 154, 87, 4545, 457,
+						454, 578, 54871124, 787445, 5451545, 541545454, 54548455, 14545454, 4545, 4545, 454, 3565,
+						78454, 115, 1, 5, 154, 5, 1545, 67, 154, 15, 1545, 1545, 154, 1545, 154545, 1545, 7878, 44, 5,
+						4, 3, 2, 4545, 54457511, 154 }));
 	}
 }

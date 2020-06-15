46f73d5937e600b0985090842a19b83919ddeeed
Rajkumar <monishrajkumar@gmail.com>
2020-06-15 14:54:30 +0530

    handled exception

--- src/main/java/com/pranavbros/service/impl/ThreadCostCalculator.java
+++ src/main/java/com/pranavbros/service/impl/ThreadCostCalculator.java
@@ -12,28 +12,32 @@ public class ThreadCostCalculator implements ThreadCostCalculatorService{
 	private static final Logger logger = LoggerFactory.getLogger(ThreadCostCalculatorService.class);
 	
 	public int getThreadCost(int[] inputArray) {
-		int arrayLength = inputArray.length;
-		if(arrayLength<2) {
-			return inputArray[0];
-		}else { //(inputArray.length>1) 
-			
-			long startTime = System.currentTimeMillis();
-			logger.info("InputArray -->{}", Arrays.toString(inputArray));
-			Quicksort qs = new Quicksort();
-			qs.sort(inputArray,0, arrayLength-1);
-			long stopTime = System.currentTimeMillis();
-			long elapsedTime = stopTime - startTime;
-			logger.info("Quicksort Time Taken -->{}", elapsedTime);
-			logger.info("SortedArray -->{}" , Arrays.toString(inputArray));
-			
-			int sum = inputArray[0] + inputArray[1];
-			int stepsSum = 0;
-			for(int i=2; i<inputArray.length; i++) {
-				stepsSum = stepsSum+sum;
-				sum = sum + inputArray[i];
+		try {
+			int arrayLength = inputArray.length;
+			if(arrayLength<2) {
+				return inputArray[0];
+			}else {
+				long startTime = System.currentTimeMillis();
+				logger.info("InputArray -->{}", Arrays.toString(inputArray));
+				Quicksort qs = new Quicksort();
+				qs.sort(inputArray,0, arrayLength-1);
+				long stopTime = System.currentTimeMillis();
+				long elapsedTime = stopTime - startTime;
+				logger.info("Quicksort Time Taken -->{}", elapsedTime);
+				logger.info("SortedArray -->{}" , Arrays.toString(inputArray));
+				
+				int sum = inputArray[0] + inputArray[1];
+				int stepsSum = 0;
+				for(int i=2; i<inputArray.length; i++) {
+					stepsSum = stepsSum+sum;
+					sum = sum + inputArray[i];
+				}
+				return (sum+stepsSum);
 			}
-			return (sum+stepsSum);
+		} catch (Exception e) {
+			logger.error("Unexpected Error-->{}" , e.getMessage());
 		} 
+		return 0;
 	}
 	
 }
